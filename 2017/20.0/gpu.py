989fc92f16dd34ac7ea98ca08cfe0ace08f86447
Thurstan Gray <grayter@gmail.com>
2017-12-20 18:06:15 +0000

    Particle swarm

---
+++ 2017/20.0/gpu.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import sys
+
+def parse(data):
+    elem = data[3:-1]
+    elem = elem.split(",")
+    elem = map(int, elem)
+    return elem
+
+def dist(pos):
+    return sum(map(abs, pos[1]))
+
+def minitem(swarm):
+    (val, idx) = min(enumerate([x[0] for x in swarm]), key=dist)
+    print val, idx
+    return idx
+
+def stepParticle(particle):
+    for i in xrange(0, 3):
+        particle[1][i] += particle[2][i]
+
+    for i in xrange(0, 3):
+        particle[0][i] += particle[1][i]
+
+def stepSwarm(swarm):
+    for p in swarm:
+        stepParticle(p)
+        
+swarm = []
+
+with open(sys.argv[1]) as f:
+    data = f.readlines()
+    for particle in data:
+        particle = particle.strip()
+        parts = particle.split(", ")
+        pos = parse(parts[0])
+        vel = parse(parts[1])
+        acc = parse(parts[2])
+        swarm.append([pos,vel,acc])
+        
+for i in xrange(0, 10000000):    
+    print minitem(swarm)
+    stepSwarm(swarm)
+
